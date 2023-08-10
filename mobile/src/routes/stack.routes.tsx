c1f65dd2dedaff38e1acf7be092680a274da202f
PEDRO HENRIQUE FLÔRES DA ROCHA <pedro_hf_rocha@estudante.sesisenai.org.br>
2023-08-10 07:53:34 -0300

    10/08

--- mobile/src/routes/stack.routes.tsx
+++ mobile/src/routes/stack.routes.tsx
@@ -2,17 +2,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const { Screen, Navigator} = createNativeStackNavigator();
 
-import { ScreenA } from '../screens/PastaA/ScreenA';
-import { ScreenB } from '../screens/PastaB/ScreenB';
-import {ScreenC } from '../screens/PastaC/ScreenC';
-import {ScreenD } from  '../screens/PastaD/ScreenD';
-import {ScreenE } from  '../screens/PastaE/ScreenE';
-import {ScreenF } from  '../screens/PastaF/ScreenF';
-import {ScreenG } from '../screens/PastaG/ScreenG';
-import {ScreenH } from  '../screens/PastaH/ScreenH';
-import {ScreenI } from '../screens/PastaI/ScreenI';
-import {ScreenJ } from '../screens/PastaJ/ScreenJ';
-import {ScreenK } from '../screens/PastaK/ScreenK';
+import { Login } from '../screens/Login/login';
+import { Register } from '../screens/Register/register';
+import { Forgetpassword} from '../screens/Forgetpassword/forgetpassword';
+import { Sendemail} from '../screens/Sendemail/sendemail';
+import { Home} from '../screens/Home/home';
+import { Profile } from '../screens/Profile/profile';
+import { Environments  } from '../screens/Environments /environments ';
+import { Environmentsprofile } from '../screens/Environmentsprofile/environmentsprofile';
+import { Product  } from '../screens/Product /product ';
+import { Productdetails  } from '../screens/Productdetails /productdetails ';
+import { Productprofile  } from '../screens/Productprofile /productprofile ';
 
 export function StackRoutes(){
     return(
