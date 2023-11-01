f0a07b1ee23e32b4d571a3c94c48a09fc9478517
PEDRO HENRIQUE FLÔRES DA ROCHA <pedro_hf_rocha@estudante.sesisenai.org.br>
2023-11-01 12:22:41 -0300

    new react project

---
+++ web-novo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+// import './App.css'
+
+export function App() {
+
+  return (
+    <>
+      <img src={} className='imgLogo'/> 
+      <div className="container">
+        <h1> Bem Vindo</h1>
+      </div>
+    </>
+  )
+
+}
+
