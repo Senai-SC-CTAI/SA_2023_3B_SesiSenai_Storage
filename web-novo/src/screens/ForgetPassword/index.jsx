e226f8432c3c183fa2f6720aa8a042b3ecc0a3e0
Pedro Flores <pedrohfd.rocha@gmail.com>
2023-11-18 18:34:45 -0300

    Finalizando ForgetPassword

--- web-novo/src/screens/ForgetPassword/index.jsx
+++ web-novo/src/screens/ForgetPassword/index.jsx
@@ -1,32 +1,63 @@
 import { NavLink } from 'react-router-dom'
 import './styles.css'
+import Logo from "../../assets/Group 10.png";  
+import { Footer } from "../../components/Footer"; 
+
 
 export function Password() {
     return (
         <>
-        <div className="mainContainer">
-
-            <div className="form-container">
-                <div className="logo-container">
-                    Esqueci minha senha
-                </div>
-
-                <form className="form">
-                    <div className="form-group">
-                        <label for="email">Email</label>
-                        <input type="text" id="email" name="email" placeholder="Enter your email" required=""/>
+            <div className="mainContainer">
+            <img src={Logo} className='mainLogo' />
+                <div className="form-container">
+                    <div className='centro'>
+                    <div className="logo-container">
+                        <div className='titulo'>
+                        Esqueci minha senha
+                        </div>
                     </div>
 
-                    <button className="form-submit-btn" type="submit">Enviar Email</button>
-                </form>
+                    <form className="form">
+                        <div className="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" id="email" name="email" placeholder="Enter your email" required="" />
+                        </div>
 
-                <NavLink to="/" className="back">Voltar</NavLink>
-                <p className="signup-link">
-                    Você não possui uma conta?
-                    <NavLink to="/register" className="signup-link link"> Crie sua conta</NavLink>
-                </p>
+    </form></div>
+                        <div className="enviar">
+                            <div className="box-1">
+                                <div className="btn btn-one">
+                                    <span>Enviar</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    <div className="voltar">
+              <NavLink to="/">
+                <div className="box-1">
+                  <div className="btn btn-one">
+                    <span>Voltar</span>
+                  </div>
+                </div>
+              </NavLink>
             </div>
-        </div>
+
+                
+
+                 
+                    <p className="signup-link">
+                        Você não possui uma conta?
+                        <NavLink to="/register" className="signup-link link"> Crie sua conta</NavLink>
+                    </p>
+                    
+                </div>
+                <div className='footer'>
+                   <Footer />
+                   </div>
+            </div>
+
+         
+            
         </>
     )
 }
