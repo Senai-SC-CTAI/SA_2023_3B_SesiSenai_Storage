d526f610334496290836e99bedacdf58dfbed376
Midir0 <lucas.franco.xrl8@gmail.com>
2023-11-18 15:32:57 -0300

    primeira versão da busca de ambientes da web (ainda com erros)

--- mobile/src/screens/ScreenEnvironments/index.tsx
+++ mobile/src/screens/ScreenEnvironments/index.tsx
@@ -1,44 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, TextInput, TouchableOpacity, Text, Image } from 'react-native';
-
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
-import styles from './styles'
+import styles from './styles';
 
 export function Environments() {
   const navigation = useNavigation();
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+  const [noResults, setNoResults] = useState(false);
+  const [searchError, setSearchError] = useState('');
 
-  function openScreen() {
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get('http://localhost:8090/salas');
+      const data = response.data;
+
+      console.log('Resposta do servidor ao buscar ambientes:', data);
+
+      if (data.length === 0) {
+        setNoResults(true);
+        setSearchError('');
+      } else {
+        setResults(data);
+        setNoResults(false);
+        setSearchError('');
+      }
+    } catch (error) {
+      console.error('Erro ao buscar ambientes:', error);
+      setSearchError('Erro ao buscar ambientes.');
+    }
+  };
+
+  const handleButtonClick = async () => {
+    try {
+      if (query.trim() !== '') {
+        const response = await axios.get(`http://localhost:8090/salas?query=${query}`);
+        const data = response.data;
+
+        console.log('Resposta do servidor ao buscar ambientes por query:', data);
+
+        if (data.length === 0) {
+          setNoResults(true);
+          setSearchError('');
+        } else {
+          setResults(data);
+          setNoResults(false);
+          setSearchError('');
+        }
+      } else {
+        setSearchError('A consulta de pesquisa está vazia. Por favor, insira um termo de pesquisa.');
+        setNoResults(true);
+        setResults([]);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar ambientes por query:', error);
+      setSearchError('Erro ao buscar ambientes por query.');
+    }
+  };
+
+  const handleOpenScreen = () => {
     navigation.navigate('Home');
-  }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Listagem de Ambientes:</Text>
 
-      {/* input search */}
-      <View style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+      {/* Input search */}
+      <View style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <View style={styles.inputSearchHolder}>
-          <TextInput placeholder="Pesquisar por Ambientes" style={{ width: '90%', fontStyle: 'italic', color: 'grey' }}></TextInput>
-          <Image
-            source={require("../../../assets/search.svg")}
-            style={{
-              width: 25,
-              height: 25,
-              resizeMode: "contain",
-              marginLeft: 53,
-            }}
+          <TextInput
+            placeholder="Pesquisar por Ambientes"
+            value={query}
+            onChangeText={(text) => setQuery(text)}
+            style={{ width: '90%', fontStyle: 'italic', color: 'grey' }}
           />
+          <TouchableOpacity style={{ width: 25, height: 25, resizeMode: 'contain', marginLeft: 10 }} onPress={handleButtonClick}>
+            <Image source={require('../../../assets/search.svg')} style={{ width: '100%', height: '100%' }} />
+          </TouchableOpacity>
         </View>
       </View>
 
-      <View style={styles.card}>
-        <Text style={styles.props}>Ex: F13</Text>
-      </View>
-      <TouchableOpacity style={styles.button} onPress={(openScreen)}>
+      {!!searchError && <Text style={styles.errorMessage}>{searchError}</Text>}
+
+      {noResults ? (
+        <Text>Nenhum ambiente encontrado com o nome "{query}".</Text>
+      ) : (
+        <View style={styles.searchResults}>
+          {results.map((result, index) => (
+            <Text key={index}>{result.nome}</Text>
+          ))}
+        </View>
+      )}
+
+      <TouchableOpacity style={styles.button} onPress={handleOpenScreen}>
         <Text style={styles.buttonText}>Voltar</Text>
       </TouchableOpacity>
-
     </View>
   );
 }
